9ef524b5d296c3b87e26b2e2fd7520d2b1f1165c
tamiressa <4serimat@gmail.com>
2023-02-22 16:21:47 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,129 +1,37 @@
--- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
---
--- Host: localhost    Database: bisa_teste_2
--- ------------------------------------------------------
--- Server version	8.0.31
+CREATE DATABASE bisa_teste_2;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE bisa_teste_2;
 
---
--- Table structure for table `entradas`
---
+CREATE TABLE Tipos_Entradas (
+id_tipo_entrada INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+nome VARCHAR(255)
+);
 
-DROP TABLE IF EXISTS `entradas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `entradas` (
-  `id_entrada` int NOT NULL AUTO_INCREMENT,
-  `id_tipo_entrada` int DEFAULT NULL,
-  `descricao` varchar(255) DEFAULT NULL,
-  `data_hora_entrada` datetime DEFAULT NULL,
-  `valor_entrada` double DEFAULT NULL,
-  PRIMARY KEY (`id_entrada`),
-  KEY `fk_entrada_tipo_entrada` (`id_tipo_entrada`),
-  CONSTRAINT `fk_entrada_tipo_entrada` FOREIGN KEY (`id_tipo_entrada`) REFERENCES `tipos_entradas` (`id_tipo_entrada`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE Tipos_Saidas (
+id_tipo_saida INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+nome VARCHAR(255)
+);
 
---
--- Dumping data for table `entradas`
---
+CREATE TABLE Entradas (
+id_entrada INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+id_tipo_entrada INT,
+descricao VARCHAR(255),
+data_hora_entrada DATETIME,
+valor_entrada DOUBLE
+);
 
-LOCK TABLES `entradas` WRITE;
-/*!40000 ALTER TABLE `entradas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `entradas` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE Saidas (
+id_saida INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+id_tipo_saida INT,
+descricao VARCHAR(255),
+data_hora_saida DATETIME,
+valor_saida DOUBLE
+);
 
---
--- Table structure for table `saidas`
---
+ALTER TABLE Entradas
+ADD CONSTRAINT fk_entrada_tipo_entrada
+FOREIGN KEY (id_tipo_entrada) REFERENCES Tipos_Entradas(id_tipo_entrada);
 
-DROP TABLE IF EXISTS `saidas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `saidas` (
-  `id_saida` int NOT NULL AUTO_INCREMENT,
-  `id_tipo_saida` int DEFAULT NULL,
-  `descricao` varchar(255) DEFAULT NULL,
-  `data_hora_saida` datetime DEFAULT NULL,
-  `valor_saida` double DEFAULT NULL,
-  PRIMARY KEY (`id_saida`),
-  KEY `fk_saida_tipo_saida` (`id_tipo_saida`),
-  CONSTRAINT `fk_saida_tipo_saida` FOREIGN KEY (`id_tipo_saida`) REFERENCES `tipos_saidas` (`id_tipo_saida`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `saidas`
---
-
-LOCK TABLES `saidas` WRITE;
-/*!40000 ALTER TABLE `saidas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `saidas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tipos_entradas`
---
-
-DROP TABLE IF EXISTS `tipos_entradas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tipos_entradas` (
-  `id_tipo_entrada` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_tipo_entrada`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tipos_entradas`
---
-
-LOCK TABLES `tipos_entradas` WRITE;
-/*!40000 ALTER TABLE `tipos_entradas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tipos_entradas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tipos_saidas`
---
-
-DROP TABLE IF EXISTS `tipos_saidas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tipos_saidas` (
-  `id_tipo_saida` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_tipo_saida`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tipos_saidas`
---
-
-LOCK TABLES `tipos_saidas` WRITE;
-/*!40000 ALTER TABLE `tipos_saidas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tipos_saidas` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-02-22 16:06:34
+ALTER TABLE Saidas
+ADD CONSTRAINT fk_saida_tipo_saida
+FOREIGN KEY (id_tipo_saida) REFERENCES Tipos_Saidas(id_tipo_saida);
